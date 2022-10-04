fafe5782454b506de1298d56ef287c5932fc6886
Ayange Felix <ayangefelix8@gmail.com>
2022-10-04 10:44:58 +0100

    complete ingredients endpoint

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,6 +1,8 @@
+from pyexpat import model
 from django.contrib import admin
 from core import models
 
 # Register your models here.
 
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
